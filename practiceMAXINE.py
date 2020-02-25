6f88e86dd613a75c73ab764848e668513316cf66
James Tracy <tracyam@Jamess-MacBook-Pro.local>
2020-02-25 11:43:01 -0800

    added last names

--- practiceMAXINE.py
+++ practiceMAXINE.py
@@ -1,5 +1,5 @@
-# Partner 1 Name: Maxine
-# Partner 2 Name: Amanda
+# Partner 1 Name: Maxine Matheson-Lieber
+# Partner 2 Name: Amanda Tracy
 #############################
 def getNRandom(n):
 	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
